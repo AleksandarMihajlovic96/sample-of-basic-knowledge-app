27d3d67eb5e54ebdeb1a0de89e7a64b4ed9bdcb3
Aleksandar <aleksandar.mihajlovic96@yahoo.com>
2022-08-29 13:35:47 +0200

    Initial project setup and added some functionalities

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import UserSearch from "./refs/UserSearch";
+
+function App() {
+  return (
+    <div>
+      {/* <Parent /> */}
+      <UserSearch />
+    </div>
+  );
+}
+
+export default App;
